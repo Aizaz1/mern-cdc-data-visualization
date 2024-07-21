722fc4fa2ec2f4203704ad38c47e23d04a174e27
Aizaz1 <aizaz.nawaz22@gmail.com>
2024-07-21 17:20:13 +0500

    Connect frontend to backend and add filtering

--- client/src/components/VaccinationData.js
+++ client/src/components/VaccinationData.js
@@ -4,18 +4,37 @@ import { fetchData } from '../services/api';
 
 const VaccinationData = () => {
   const [data, setData] = useState([]);
+  const [filters, setFilters] = useState({});
 
   useEffect(() => {
     const getData = async () => {
-      const result = await fetchData();
+      const result = await fetchData(filters);
       setData(result);
     };
     getData();
-  }, []);
+  }, [filters]);
+
+  const handleFilterChange = (e) => {
+    setFilters({
+      ...filters,
+      [e.target.name]: e.target.value
+    });
+  };
 
   return (
     <div>
       <h1>Vaccination Data</h1>
+      <form>
+        <label>
+          Jurisdiction:
+          <input type="text" name="jurisdiction" onChange={handleFilterChange} />
+        </label>
+        <label>
+          Vaccination Status:
+          <input type="text" name="vaccination_status" onChange={handleFilterChange} />
+        </label>
+        <button type="submit">Apply Filters</button>
+      </form>
       <ul>
         {data.map((item, index) => (
           <li key={index}>{JSON.stringify(item)}</li>
